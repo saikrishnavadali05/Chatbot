d289d4bd1d508de4a1444f71be66bfac008a9215
saikrishnavadali <saikrishna.vadali@lokibots.com>
2021-01-23 09:53:05 +0530

    first execution

--- chatgui.py
+++ chatgui.py
@@ -1,14 +1,15 @@
+from nltk.stem import WordNetLemmatizer
+import keras
+from keras.models import load_model
 
 import nltk
-from nltk.stem import WordNetLemmatizer
-lemmatizer = WordNetLemmatizer()
 import pickle
 import numpy as np
-
-from keras.models import load_model
-model = load_model('chatbot_model.h5')
 import json
 import random
+
+model = load_model('chatbot_model.h5')
+lemmatizer = WordNetLemmatizer()
 intents = json.loads(open('intents.json').read())
 words = pickle.load(open('words.pkl','rb'))
 classes = pickle.load(open('classes.pkl','rb'))
